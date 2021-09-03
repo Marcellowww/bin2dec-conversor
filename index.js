77620ebd7182cf31234615f027e6bfca9e9c00a6
Marcelo <guedesmarcello733@gmail.com>
2021-09-03 11:29:11 -0300

    Adicionando todos os arquivos ja terminados

---
+++ index.js
@@ -0,0 +1,39 @@
+let bin;
+let dec;
+let alert;
+
+function getData(){
+    bin = document.getElementById('bin');
+    dec = document.getElementById('dec');
+    document.getElementById('alert').style.display = 'none'
+    verify();
+
+}
+
+function verify(){
+    document.getElementById('alert').style.display = 'none';
+    
+    if(bin.value == ''){
+        return
+    }
+    
+
+    bin.value.split('').map((char) => {
+        if(char !== '0' && char !== '1'){
+            document.getElementById('alert').style.display = 'block';
+            return
+        }
+        conversion()
+    })
+
+
+}
+
+function conversion(){
+    result = parseInt(bin.value, 2);
+    print()
+}
+
+function print(){
+    dec.value = result;
+}
